96289e04eb5a1ade5ce7ea3898f7eee1165b69b4
Vu Nhat Anh <anhvn@Vus-MacBook-Pro.local>
2021-07-25 21:11:19 +0700

    Add Day of the Programmer Problem

---
+++ Day_of_the_Programmer/solution.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+function dayOfProgrammer($year) {
+    // Write your code here
+    if ($year == 1918) return '26.09.1918';
+    if ($year < 1918 && $year % 4 == 0) {
+        return "12.09.{$year}";
+    } else if ((($year % 4 == 0) && ($year % 100 != 0)) || ($year % 400 == 0)) {
+        return "12.09.{$year}";
+    }
+    return "13.09.{$year}";
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$year = intval(trim(fgets(STDIN)));
+
+$result = dayOfProgrammer($year);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
+
